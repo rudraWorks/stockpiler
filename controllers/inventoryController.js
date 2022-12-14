778341f80b36e626fa20a46a66b5e255c0b53008
= <srudra754@gmail.com>
2022-12-14 13:39:10 +0530

    items data

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,11 +1,15 @@
-const { findOneAndUpdate, findOneAndDelete } = require('../models/items')
 const itemModel = require('../models/items')
 
 module.exports.dashboard = async (req,res) =>{
     if(!res.locals.isAuthenticated){
         return res.redirect('/login')
     }
-    return res.render('inventory/dashboard')
+
+    let t = await itemModel.find({username:res.locals.user},{itemPrice:1,itemQuantity:1})
+    let totalWorth = 0,totalItems= t.length 
+    for(let i=0;i<t.length;++i)
+    totalWorth+=t[i].itemPrice*t[i].itemQuantity
+    return res.render('inventory/dashboard',{totalItems,totalWorth})
 }
 
 module.exports.addItem = async (req,res) =>{
@@ -15,6 +19,12 @@ module.exports.addItem = async (req,res) =>{
     let {itemId,itemName,itemPrice,itemQuantity,itemManufacturer,itemExpiry,itemEntry} = req.body 
     
     if(!itemId || !itemName || !itemPrice || !itemQuantity || !itemManufacturer || !itemExpiry || !itemEntry) return res.json({success:false,message:"input fields can't be empty!"})
+
+    if(itemPrice<=0)
+    return res.json({success:false,message:"invalid item price!"})
+
+    if(itemQuantity<=0)
+    return res.json({success:false,message:"invalid item quantity"})
 
     try{
         await itemModel.create({username:res.locals.user,itemId,itemName,itemPrice,itemQuantity,itemManufacturer,itemExpiry,itemEntry})
@@ -31,7 +41,9 @@ module.exports.allItems = async (req,res)=>{
         return res.redirect('/login')
     }
     let t = await itemModel.find({username:res.locals.user})
-    res.render('inventory/allItems',{allItems:t})
+    let sum=0
+    for(let i=0;i<t.length;++i)sum+=t[i].itemPrice
+    res.render('inventory/allItems',{allItems:t,totalWorth:sum})
 }
 
 module.exports.deleteItem = async (req,res) =>{
